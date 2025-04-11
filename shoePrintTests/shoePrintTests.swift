642598022370de25d6c0803dc3383ea0cfb3804d
Simon NAUD <162438554+nomisduan@users.noreply.github.com>
2025-04-11 08:12:23 +0200

    Initial Commit

---
+++ shoePrintTests/shoePrintTests.swift
@@ -0,0 +1,17 @@
+//
+//  shoePrintTests.swift
+//  shoePrintTests
+//
+//  Created by Simon Naud on 11/04/2025.
+//
+
+import Testing
+@testable import shoePrint
+
+struct shoePrintTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
